ac968fca2abaad54ec20319aad30dff2ad7803a5
shaunwillis-SpectrumReach <shaun.willis@charter.com>
2017-11-27 22:24:23 -0600

    Connect QuotaGuard

--- app/Listeners/Http.js
+++ app/Listeners/Http.js
@@ -39,4 +39,27 @@ Http.handleError = function* (error, request, response) {
  * starting http server.
  */
 Http.onStart = function () {
+  var http, options, proxy, url;
+
+http = require("http");
+
+url = require("url");
+
+proxy = url.parse(process.env.QUOTAGUARDSTATIC_URL);
+target  = url.parse("http://ip.jsontest.com/");
+
+options = {
+  hostname: proxy.hostname,
+  port: proxy.port || 80,
+  path: target.href,
+  headers: {
+    "Proxy-Authorization": "Basic " + (new Buffer(proxy.auth).toString("base64")),
+    "Host" : target.hostname
+  }
+};
+
+http.get(options, function(res) {
+  res.pipe(process.stdout);
+  return console.log("status code", res.statusCode);
+});
 };
